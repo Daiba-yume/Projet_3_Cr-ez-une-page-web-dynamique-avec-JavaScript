22a325b9024e4bebb9f15c166ad57d76a1fe9abd
daiba <daiba_b@yahoo.com>
2023-03-28 14:05:31 +0200

    modification

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,54 +1,108 @@
-// Get the modal
-createModal();
+// Show and hide modal //
+const editProfile = document.getElementById("editProfile");
+const modal = document.querySelector("#modal");
+editProfile.addEventListener("click", function (event) {
+  const contentEdit = editContentModal();
+  const modalContent = document.querySelector("#modal-content");
+  modalContent.appendChild(contentEdit);
+  showModal();
+});
 
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
+const modalClose = document.querySelector("#modal-close");
+modalClose.addEventListener("click", hideModal);
+function showModal() {
   modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+}
+function hideModal() {
   modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
-function createModal() {
-  const myModal = document.createElement("div");
-  myModal.setAttribute("id", "myModal");
-  myModal.setAttribute("class", "modal");
-
-  const content = document.createElement("div");
-  content.setAttribute("class", "modal-content");
-
-  const close = document.createElement("span");
-  close.setAttribute("class", "close");
-  close.textContent = "X";
-
-  const text = document.createElement("p");
-  text.textContent = "Some text in the Modal..";
-  content.appendChild(text);
-  content.appendChild(close);
-  myModal.appendChild(content);
-
-  const section = document.getElementById("introduction");
-  section.appendChild(myModal);
 }
 
-// Empêche le click à l'intérieur du contenu (donc pas de prop vers les parents)
-const stopPropagation = function (e) {
+//************************ */
+/*
+const modalPhoto = document.querySelector("#modal-photo");
+
+modalContent.addEventListener("click", function (e) {
   e.stopPropagation();
-};
+});
+modalPhoto.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+
+modal.addEventListener("click", hideModal);
+modalContent.addEventListener("click", function (e) {
+  e.stopPropagation();
+});
+*/
+
+// affichage de la modal photo //
+/*
+const newPhotoBtn = document.querySelector("#new-photo");
+const returnBtn = document.querySelector("#modal-return");
+const modalPhotoClose = document.querySelector("#modal-photo-close");
+newPhotoBtn.addEventListener("click", function () {
+  modalContent.style.display = "none";
+  modalPhoto.style.display = "block";
+});
+returnBtn.addEventListener("click", function () {
+  modalContent.style.display = "flex";
+  modalPhoto.style.display = "none";
+});
+modalPhotoClose.addEventListener("click", hideModal);
+
+// ajout des travaux dans la modal //
+const imagesModalContainer = document.querySelector(".gallery-modal");
+/*
+const reponses = fetch("http://localhost:5678/api/works")
+  .then((reponse) => reponse.json())
+  .then((datas) => {
+    datas.forEach((works) => {
+      
+    });
+  });
+*/
+
+function editContentModal() {
+  const div = document.createElement("div");
+  // boucler sur le tableau et creer la liste des element
+  allProjects.forEach((element) => {
+    const card = createOneCard(element);
+    div.appendChild(card);
+  });
+  // Ajouter les deux bouton en bas de ajouter et supprimer
+
+  // retourne finalement une dv globale qui contient que le content de edit
+  return div;
+}
+
+function createProjectContentModal() {
+  // creer en javascript les element pour l'ajout d'un projet
+  // photo
+  // titre
+  // liste des categorie
+  // retourne finalement une dv globale qui contient que le visuel de create Project
+}
+
+function createOneCard(works) {
+  const figure = document.createElement("figure");
+  const figureCaption = document.createElement("figcaption");
+  const figureImage = document.createElement("img");
+  const deleteIcon = document.createElement("i");
+
+  figureImage.src = works.imageUrl;
+  figureImage.alt = works.title;
+  figureCaption.innerHTML = "éditer";
+  figure.setAttribute("data-id", works.id); // Add a data-id attribute to store the work ID
+  deleteIcon.className = "fa-regular fa-trash-can";
+
+  figure.appendChild(figureImage);
+  figure.appendChild(figureCaption);
+  figure.appendChild(deleteIcon);
+  // Ajout d'un event de suppression lors du click sur l'icon supprimer
+  deleteIcon.addEventListener("click", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    //deleteWorkById(works.id);
+  });
+
+  return figure;
+}
